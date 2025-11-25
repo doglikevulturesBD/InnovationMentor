07cffb05fd733047535105cd03cb06327bdc8938
doglikevulturesBD <doglikevultures@gmail.com>
2025-11-25 20:24:41 +0200

    Update bm_fusion.py

--- utils/bm_fusion.py
+++ utils/bm_fusion.py
@@ -1,1 +1,24 @@
+# utils/bm_fusion.py
+
+from typing import Dict
+
+
+def fuse_scores(
+    rule_scores: Dict[str, float],
+    ai_scores: Dict[str, float],
+    rule_weight: float = 0.7,
+) -> Dict[str, float]:
+    """
+    Hybrid score = rule_weight * rule_score + (1 - rule_weight) * ai_score
+    """
+    final: Dict[str, float] = {}
+    models = set(rule_scores.keys()) | set(ai_scores.keys())
+    ai_weight = 1.0 - rule_weight
+
+    for m in models:
+        r = rule_scores.get(m, 0.0)
+        a = ai_scores.get(m, 0.0)
+        final[m] = rule_weight * r + ai_weight * a
+
+    return final
 
